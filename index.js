5776ba5eb9a08b075b392cb65f34b545f9f75b48
Vijaygopal27 <76892347+Vijaygopal27@users.noreply.github.com>
2021-12-23 15:13:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require('express')
+const app = express()
+
+const bodyParser = require ('body-parser')
+const mongoose = require ('mongoose')
+
+mongoose.connect("mongodb://localhost:27017/myUrlShortener")
+
+const {UrlModel} = require('./models/urlshort')
+
+app.set('view engine','ejs')
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.get('/',function(req,res){
+    let allUrl = UrlModel.find(function(err,result){
+res.render('home',{
+    urlResult : result
+})
+    })
+    
+})
+
+
+app.post('/create',function(req,res){
+
+
+    let urlShort =new UrlModel({
+        longUrl : req.body.longurl,
+        shortUrl : generateUrl()
+    })
+
+    urlShort.save(function(err,data){
+        if(err) throw err;
+        res.redirect('/')
+
+
+    })
+})
+
+app.get('/:urlId',function(req,res){
+    UrlModel.findOne({shortUrl : req.params.urlId},function(err,data){
+        if(err) throw err;
+
+        UrlModel.findByIdAndUpdate({_id:data.id},{$inc:{clickCount : 1}},function(err,updatedData){
+            if(err) throw err;
+            res.redirect(data.longUrl)
+
+        })
+        
+    })
+})
+
+app.get('/delete/:id',function(req,res){
+    UrlModel.findByIdAndDelete({_id:req.params.id},function(err,deleteData){
+        if(err) throw err;
+        res.redirect('/')
+    })
+})
+
+
+app.listen(3000, function(){
+    console.log("running in 3000")
+})
+
+function generateUrl(){
+     var rndResult = "";
+     var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+     var characterslen = characters.length;
+
+     for (let i = 0; i <5; i++){
+         rndResult += characters.charAt(
+             Math.floor(Math.random()* characterslen)
+         )
+     }
+    console.log(rndResult)
+     return rndResult
+       }
+     
